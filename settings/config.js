464dd4aab15a7befc5f86319c3fb6435cd3856ff
valzeyk <98308867+valzeyk@users.noreply.github.com>
2022-02-11 22:12:33 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -5,9 +5,9 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="ZeeoneOfc"
-global.ownernumber = "62887435047326"
-global.botname = "Haruka-Bot"
+global.ownername ="ValzZx7"
+global.ownernumber = "628310150840q"
+global.botname = "ValzZ-Bot"
 global.thumbnail = fs.readFileSync("./settings/haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.limit = {
